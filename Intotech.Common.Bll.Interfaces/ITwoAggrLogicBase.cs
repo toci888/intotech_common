14ec7819f452caefd7bf1b957f74d5e2887e6859
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-06 20:50:24 +0100

    aggregation logics concept.

---
+++ Intotech.Common.Bll.Interfaces/ITwoAggrLogicBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Common.Bll.Interfaces
+{
+    public interface ITwoAggrLogicBase<TFirstLogic, TSecondLogic>
+    {
+
+    }
+}
---
+++ Intotech.Common.Bll/ITwoAggrLogicBase.cs
@@ -0,0 +1,21 @@
+﻿using Intotech.Common.Bll.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Common.Bll
+{
+    public abstract class TwoAggrLogicBase<TFirstLogic, TSecondLogic> : ITwoAggrLogicBase<TFirstLogic, TSecondLogic>
+    {
+        protected TFirstLogic FirstLogic;
+        protected TSecondLogic SecondLogic;
+
+        protected TwoAggrLogicBase(TFirstLogic firstLogic, TSecondLogic secondLogic)
+        {
+            FirstLogic = firstLogic;
+            SecondLogic = secondLogic;
+        }
+    }
+}
