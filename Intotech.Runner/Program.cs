9a08e72b4ef1b4711f18130556986950aac72c19
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-18 18:35:55 +0100

    fixes.

--- Intotech.Runner/Program.cs
+++ Intotech.Runner/Program.cs
@@ -3,10 +3,11 @@ using Intotech.ReflectiveTools.SourceGenerators;
 
 Console.WriteLine("The runner is a midnight runner ! xd");
 
-List<string> fileNames =  ModelsReader.ReadAllModelsNames(@"C:\Users\bzapa\source\repos\toci888\intotech_wheelo\Toci.Driver.Bll.Porsche.Interfaces\Toci.Driver.Database.Persistence\Models");
+List<string> fileNames =  ModelsReader.ReadAllModelsNames(@"C:\Users\bzapa\source\repos\toci888\intotech_wheelo\Toci.Driver.Bll.Porsche.Interfaces\Intotech.Wheelo.Social.Database.Persistence\Models");
 
 InterfaceAndLogicFileGenerator ifG = new InterfaceAndLogicFileGenerator();
 
-ifG.GenerateCodeFiles(@"C:\Users\bzapa\source\repos\dawid", fileNames, new GeneratedFileDto() { KeyNamespace = "Intotech.Wheelo.Bll.Persistence.Interfaces", ModelPersistenceUsing = "Toci.Driver.Database.Persistence.Models" }, false);
+//ifG.GenerateCodeFiles(@"C:\Users\bzapa\source\repos\toci888\intotech_wheelo\reflective", fileNames, new GeneratedFileDto() { KeyNamespace = "Intotech.Wheelo.Social.Bll.Persistence.Interfaces", ModelPersistenceUsing = "Intotech.Wheelo.Social.Database.Persistence.Models" }, true);
+ifG.GenerateCodeFiles(@"C:\Users\bzapa\source\repos\toci888\intotech_wheelo\reflective", fileNames, new GeneratedFileDto() { KeyNamespace = "Intotech.Wheelo.Social.Bll.Persistence", ModelPersistenceUsing = "Intotech.Wheelo.Social.Database.Persistence.Models" }, false);
 
 Console.WriteLine(fileNames.Count);
