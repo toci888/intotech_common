2b71e29926a46fad3619c1399836c75b645db491
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-19 19:53:14 +0100

    own ts from c# models tools - works!

--- Intotech.ReflectiveTools/SourceGenerators/ModelsToTypescript/TypeScriptModelsGenerator.cs
+++ Intotech.ReflectiveTools/SourceGenerators/ModelsToTypescript/TypeScriptModelsGenerator.cs
@@ -9,6 +9,18 @@ namespace Intotech.ReflectiveTools.SourceGenerators.ModelsToTypescript
 {
     public class TypeScriptModelsGenerator
     {
+        protected Dictionary<string, string> TypesMap = new Dictionary<string, string>()
+        {
+            { "Int32", "number" },
+            { "String", "string" },
+
+        };
+
+        protected List<string> skip = new List<string>()
+        {
+            "Navigation"
+        };
+
         public void Generate(Assembly assembly)
         {
             List<Type> types = assembly.GetTypes().ToList();
@@ -21,12 +33,38 @@ namespace Intotech.ReflectiveTools.SourceGenerators.ModelsToTypescript
 
                 foreach (PropertyInfo property in properties)
                 { //Id!: number;
-                    json += property.Name + "!: " + property.PropertyType + "; " + Environment.NewLine;
+                    if (!NameContains(property.Name))
+                    {
+                        string mappedType = GetTypeName(property.PropertyType.ToString());  //  TypesMap.Where(map => property.PropertyType.ToString().Contains(map.Key) ? map.Value : string.Empty)
+
+                        if (!string.IsNullOrEmpty(mappedType))
+                        {
+                            json += property.Name + "!: " + mappedType + "; " + Environment.NewLine;
+                        }
+                    }
                 }
 
                 string final = "export class " + type.Name + " {" + Environment.NewLine + json + Environment.NewLine + "}";
                 File.WriteAllText(@"C:\Users\bzapa\source\repos\toci888\intotech_wheelo\reflective\" + type.Name + ".ts", final);
             }
+        }
+
+        protected virtual bool NameContains(string name)
+        {
+            return skip.Where(x => name.Contains(x)).Any();
+        }
+
+        protected virtual string GetTypeName(string type)
+        {
+            foreach (KeyValuePair<string, string> map in TypesMap)
+            {
+                if (type.Contains(map.Key))
+                {
+                    return map.Value;
+                }
+            }
+
+            return string.Empty;
         }
     }
 }
