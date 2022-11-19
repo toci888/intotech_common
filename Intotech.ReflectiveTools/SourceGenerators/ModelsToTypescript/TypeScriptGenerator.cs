1616357d11dc11b7589f7a8b171dae9e9678ce7b
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-19 17:05:57 +0100

    typescript generator 1st approach.

---
+++ Intotech.ReflectiveTools/SourceGenerators/ModelsToTypescript/TypeScriptGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TypeLite;
+using TypeLite.TsModels;
+
+namespace Intotech.ReflectiveTools.SourceGenerators.ModelsToTypescript
+{
+    public class TypeScriptGenerator
+    {
+        public void GenerateTsModels(Assembly asembly)
+        {
+            //TypeScript.Definitions().Generate();
+
+            TsGenerator tsg = new TsGenerator();
+
+            List<Type> types = asembly.GetTypes().ToList();
+            List< TsClass > typesList = new List< TsClass >();
+
+            foreach (Type type in types)
+            {
+                typesList.Add(new TsClass(type));
+            }
+
+            //List<TsModel> tsModels = new List<TsModel>();
+
+            TsModel tsModel = new TsModel(typesList);
+
+            string result = tsg.Generate(tsModel, TsGeneratorOutput.Properties);
+        }
+    }
+}
