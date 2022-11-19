0928828ec91b727b336e806537ef4fe389495e1a
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-19 14:25:45 +0100

    generic DtoModelMapper -> wrapper for aoutomapper.

---
+++ Intotech.Common/DtoModelMapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Common
+{
+    public static class DtoModelMapper
+    {
+        public static TDto Map<TDto, TModel>(TModel model) where TDto : new()
+        {
+            MapperConfiguration config = new MapperConfiguration(cfg =>
+                cfg.CreateMap<TModel, TDto>());
+
+            Mapper mapper = new Mapper(config);
+
+            return mapper.Map<TDto>(model);
+        }
+    }
+}
