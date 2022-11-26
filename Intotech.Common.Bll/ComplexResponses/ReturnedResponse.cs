ba73d88a9c01e2c9c24e84bf5dd4f4cb0813ff94
Julia <Julia@Intotech>
2022-11-26 16:56:57 +0000

    int i.e. 4 digit code, returned response adjustment.

--- Intotech.Common.Bll/ComplexResponses/ReturnedResponse.cs
+++ Intotech.Common.Bll/ComplexResponses/ReturnedResponse.cs
@@ -2,6 +2,13 @@
 
 public class ReturnedResponse<TResult>
 {
+    public ReturnedResponse(TResult result, string errorMessage, bool isSuccess)
+    {
+        MethodResult= result;
+        IsSuccess= isSuccess;
+        ErrorMessage= errorMessage;
+    }
+
     public TResult MethodResult { get; set; }
     public string ErrorMessage { get; set; }
     public bool IsSuccess { get; set; }
---
+++ Intotech.Common/IntUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Common
+{
+    public static class IntUtils
+    {
+        public static int GetRandomCode(int min, int max)
+        {
+            Random rnd = new Random();
+
+            return rnd.Next(min, max);
+        }
+    }
+}
