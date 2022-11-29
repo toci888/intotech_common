90f4b3b1328fb4f795ca8cc44c59b41f97ac44b8
Bartomiej Zapart <bzapart@hotmail.com>
2022-11-29 18:10:19 +0100

    4 constructor parameter.

--- Intotech.Common.Bll/ComplexResponses/ReturnedResponse.cs
+++ Intotech.Common.Bll/ComplexResponses/ReturnedResponse.cs
@@ -2,14 +2,16 @@
 
 public class ReturnedResponse<TResult>
 {
-    public ReturnedResponse(TResult result, string errorMessage, bool isSuccess)
+    public ReturnedResponse(TResult result, string errorMessage, bool isSuccess, int errorCode)
     {
-        MethodResult= result;
-        IsSuccess= isSuccess;
-        ErrorMessage= errorMessage;
+        MethodResult = result;
+        IsSuccess = isSuccess;
+        ErrorMessage = errorMessage;
+        ErrorCode = errorCode;
     }
 
     public TResult MethodResult { get; set; }
     public string ErrorMessage { get; set; }
     public bool IsSuccess { get; set; }
+    public int ErrorCode { get; set; }
 }
