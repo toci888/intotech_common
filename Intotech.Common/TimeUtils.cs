2f8fc71e9b309620fa8d347812f11a4ee1023e05
Bartomiej Zapart <bzapart@hotmail.com>
2023-01-09 09:49:27 +0100

    time util for hour and miunet to prepend 0.

---
+++ Intotech.Common/TimeUtils.cs
@@ -0,0 +1,16 @@
+﻿namespace Intotech.Common;
+
+public static class TimeUtils
+{
+    public static string GetCorrectTime(int hourMinute)
+    {
+        if (hourMinute < 10)
+        {
+            return "0" + hourMinute;
+        }
+
+        return hourMinute.ToString();
+    }
+
+
+}
