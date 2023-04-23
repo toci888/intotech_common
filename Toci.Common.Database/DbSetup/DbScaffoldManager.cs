cfe2a1f7fb8d61bd521e1c970921f681caa1b09c
Kacper Wybierala <kacper.wybierala1@gmail.com>
2023-04-23 22:58:06 +0200

    Create database automatically

---
+++ Toci.Common.Database/DbSetup/DbScaffoldManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+
+namespace Intotech.Common.Database.DbSetup;
+
+public class DbScaffoldManager
+{
+    private string _connectionString;
+    private string _projectName;
+    private string _parentProjectFolderPath;
+
+    public DbScaffoldManager(string connectionString, string projectName, string parentProjectFolderPath)
+    {
+        _connectionString = connectionString;
+        _projectName = projectName;
+        _parentProjectFolderPath = parentProjectFolderPath;
+    }
+
+    public void RunScaffold()
+    {
+        string solutionDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        string workingDirectory = Path.Combine(solutionDirectory, _parentProjectFolderPath);
+
+        var arguments = $"ef dbcontext scaffold \"{_connectionString}\" Npgsql.EntityFrameworkCore.PostgreSQL -o Models --project {_projectName} -f";
+        var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WorkingDirectory = workingDirectory
+            }
+        };
+        process.Start();
+        process.WaitForExit();
+
+        if (process.ExitCode != 0)
+        {
+            string errorOutput = process.StandardError.ReadToEnd();
+            Console.WriteLine($"Error: {errorOutput}");
+        }
+        else
+        {
+            Console.WriteLine("Models generated successfully.");
+        }
+    }
+}
---
+++ Toci.Common.Database/DbSetup/DbSetupManager.cs
@@ -0,0 +1,78 @@
+﻿using Npgsql;
+
+namespace Intotech.Common.Database.DbSetup;
+
+public class DbSetupManager
+{
+    private string _connectionString;
+    private string _sqlFilePath;
+    private string _databaseName;
+    private string _customDbConnectionString;
+
+    protected string FileContents;
+
+    public DbSetupManager(string connectionString, string customDbConnectionString,string databaseName, string sqlFilePath)
+    {
+        _connectionString = connectionString;
+        _sqlFilePath = sqlFilePath;
+        _databaseName = databaseName;
+        _customDbConnectionString = customDbConnectionString;
+    }
+
+    public bool SetupDatabase()
+    {
+        using NpgsqlConnection connection = new NpgsqlConnection(_connectionString);
+        connection.Open();
+
+        using (NpgsqlCommand command = new NpgsqlCommand($"CREATE DATABASE \"{_databaseName}\"", connection))
+        {
+            try
+            {
+                command.ExecuteNonQuery();
+            }
+            catch (NpgsqlException ex)
+            {
+                if (ex.SqlState == "42P04")
+                {
+                    Console.WriteLine($"Database '{_databaseName}' already exists.");
+                }
+                else
+                {
+                    Console.WriteLine("Error creating database: " + ex.Message);
+                }
+            }
+        }
+
+        connection.Close();
+
+        using (NpgsqlConnection customConnection = new NpgsqlConnection(_customDbConnectionString))
+        {
+            customConnection.Open();
+
+            ReadSqlFile();
+
+            using (NpgsqlCommand command = new NpgsqlCommand(FileContents, customConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+
+            customConnection.Close();
+        }
+
+        return true;
+    }
+
+    protected virtual bool ReadSqlFile()
+    {
+        try
+        {
+            FileContents = File.ReadAllText(_sqlFilePath);
+        }
+        catch (FileNotFoundException e)
+        {
+            
+        }
+        
+        return true;
+    }
+}
