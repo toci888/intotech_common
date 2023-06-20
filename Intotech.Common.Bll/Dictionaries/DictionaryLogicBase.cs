7fb0d57514db0b2697033777a3de3c8221941bf6
Bartłomiej Zapart <Bartlomiej.Zapart@grupawp.pl>
2023-06-20 21:42:01 +0200

    prepared for xerion code generation.

--- Intotech.Common.Bll/Dictionaries/DictionaryLogicBase.cs
+++ Intotech.Common.Bll/Dictionaries/DictionaryLogicBase.cs
@@ -4,5 +4,8 @@ namespace Intotech.Common.Bll.Dictionaries;
 
 public abstract class DictionaryLogicBase<TDictionaryModel> : LogicBase<TDictionaryModel> where TDictionaryModel : DictionaryModelBase
 {
-
+    public virtual IEnumerable<TDictionaryModel> GetDictionaryItems(string filter)
+    {
+        return Select(m => m.Name.StartsWith(filter));
+    }
 }
