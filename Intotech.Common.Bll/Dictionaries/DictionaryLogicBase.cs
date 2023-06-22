50d7cc76d19d93fe86d8ab216b90ccc11d00d914
Bartłomiej Zapart <Bartlomiej.Zapart@grupawp.pl>
2023-06-22 16:48:10 +0200

    dafaq

--- Intotech.Common.Bll/Dictionaries/DictionaryLogicBase.cs
+++ Intotech.Common.Bll/Dictionaries/DictionaryLogicBase.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Intotech.Common.Bll.Dictionaries;
 
 public abstract class DictionaryLogicBase<TDictionaryModel> : LogicBase<TDictionaryModel> where TDictionaryModel : DictionaryModelBase
 {
-    public virtual IEnumerable<TDictionaryModel> GetDictionaryItems(string filter)
+    public virtual IEnumerable<TDictionaryModel> GetDictionaryItems(Expression<Func<TDictionaryModel, bool>> filter)
     {
-        return Select(m => m.Name.StartsWith(filter));
+        return Select(filter);
     }
 }
