bdbe24aace36646a190f7ffce24c0dc4c969b6bd
kacper <kacper.wybierala1@gmail.com>
2023-06-22 17:03:25 -0700

    dictionaries works

---
+++ Intotech.Common.Bll.Interfaces/IDictionaryLogicBase.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq.Expressions;
+
+namespace Intotech.Common.Bll.Interfaces;
+
+public interface IDictionaryLogicBase<TModel> : ILogicBase<TModel> where TModel : class
+{
+    IEnumerable<TModel> GetDictionaryItems(Expression<Func<TModel, bool>> filter);
+}
--- Intotech.Common.Bll/Dictionaries/DictionaryLogicBase.cs
+++ Intotech.Common.Bll/Dictionaries/DictionaryLogicBase.cs
@@ -3,7 +3,7 @@ using System.Linq.Expressions;
 
 namespace Intotech.Common.Bll.Dictionaries;
 
-public abstract class DictionaryLogicBase<TDictionaryModel> : LogicBase<TDictionaryModel> where TDictionaryModel : DictionaryModelBase
+public abstract class DictionaryLogicBase<TDictionaryModel> : LogicBase<TDictionaryModel> where TDictionaryModel : class // where TDictionaryModel : DictionaryModelBase
 {
     public virtual IEnumerable<TDictionaryModel> GetDictionaryItems(Expression<Func<TDictionaryModel, bool>> filter)
     {
