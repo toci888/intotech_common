91d8bf5b2bddd652f95e7a0041f8f873d58438f0
Kacper W <kacper.wybierala1@gmail.com>
2023-07-26 21:55:27 +0300

    errorlogiing improvement.

--- Intotech.Common/IErrorLogger.cs
+++ Intotech.Common/IErrorLogger.cs
@@ -3,5 +3,8 @@
 public interface IErrorLogger
 {
     public void Log(List<Exception> ex);
+    public void Log(Exception ex, LogLevels logLevel);
     public void Log(string message);
+    public void Log(string message, LogLevels logLevel);
+    public void Log(string message, LogLevels logLevel, params object[] details);
 }
