c3cb689fb060da3f1617fdecea994ecb4786f869
toci888 <bzapart@gmail.com>
2023-07-27 14:13:17 +0200

    translation engine scratch.

---
+++ Intotech.Common.Interfaces/ITranslationEngineI18n.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Common.Interfaces
+{
+    public interface ITranslationEngineI18n
+    {
+        string Translate(string langCode, string tag);
+    }
+}
---
+++ Intotech.Common/TranslationEngineI18n.cs
@@ -0,0 +1,24 @@
+﻿using Intotech.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.Common
+{
+    public abstract class TranslationEngineI18n : ITranslationEngineI18n
+    {
+        protected Dictionary<string, Dictionary<string, string>> ApplicationTranslationData;
+
+        protected TranslationEngineI18n(Dictionary<string, Dictionary<string, string>> applicationTranslationData)
+        {
+            ApplicationTranslationData = applicationTranslationData;
+        }
+
+        public virtual string Translate(string langCode, string tag)
+        {
+            return ApplicationTranslationData.ContainsKey(langCode) && ApplicationTranslationData[langCode].ContainsKey(tag) ? ApplicationTranslationData[langCode][tag] : tag;
+        }
+    }
+}
