3f39f3a44e8c365481f876a96dd36a4e28421b44
toci888 <bzapart@gmail.com>
2023-09-07 12:15:06 +0200

    ilogichandlergen init.

---
+++ Intotech.GhostRider/Intotech.GhostRider/Panels/IDtoLogicHandlerPanel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.GhostRider.Panels
+{
+    public class IDtoLogicHandlerPanel : ILogicGenerationPanel
+    {
+        protected override void HandleClick(object? sender, EventArgs eventArgs)
+        {
+           // base.HandleClick(sender, eventArgs);
+        }
+    }
+}
