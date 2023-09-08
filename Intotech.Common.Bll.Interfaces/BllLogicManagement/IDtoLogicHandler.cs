c20bd6e231546292b1bcc4f6d7d2da3b90cdcd3c
toci888 <bzapart@gmail.com>
2023-09-08 08:44:33 +0200

    fixed temporarily derivance of idictionary

--- Intotech.Common.Bll.Interfaces/BllLogicManagement/IDtoLogicHandler.cs
+++ Intotech.Common.Bll.Interfaces/BllLogicManagement/IDtoLogicHandler.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Intotech.Common.Bll.Interfaces.BllLogicManagement
 {
-    public interface IDtoLogicHandler<TDtoLogic, TDto> : IDictionary<string, TDtoLogic>  //where TDtoLogic : IAbsDtoLogic<TDto> where TDto : class
+    public interface IDtoLogicHandler<TDtoLogic, TDto> //: IDictionary<string, TDtoLogic>  //where TDtoLogic : IAbsDtoLogic<TDto> where TDto : class
     {
         TDto GetById(int id, List<string> handlers, TDto dto);
 
