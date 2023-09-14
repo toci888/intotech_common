59b2d88d89aad50cb7b7085521c755d16b9d4772
toci888 <bzapart@gmail.com>
2023-09-14 19:22:35 +0200

    scratch of api proxy generator.

---
+++ Intotech.ReflectiveTools/ApiProxyGenerators/ApiProxyGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.ReflectiveTools.ApiProxyGenerators
+{
+    public class ApiProxyGenerator
+    {
+        protected Assembly ControllersAssembly;
+
+        public ApiProxyGenerator()
+        {
+            ControllersAssembly = Assembly.LoadFrom(@"C:\Users\bzapa\source\repos\toci888\intotech_wheelo\Toci.Driver.Api\bin\Debug\net7.0\Intotech.Wheelo.Api.dll");
+        }
+
+        public virtual void CreateProxies()
+        {
+            List<Type> controllerTypes = ControllersAssembly.GetTypes().Where(t => t.Name.Contains("Controller")).ToList();
+
+            foreach (Type controllerType in controllerTypes)
+            {
+                List<MethodInfo> methods = controllerType.GetMethods().ToList();
+
+                foreach (MethodInfo method in methods)
+                {
+                    GetProxyBody(method);
+                }
+            }
+        }
+
+        protected virtual string GetProxyBody(MethodInfo method)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"public virtual {method.ReturnType.GenericTypeArguments[0].Name} {method.Name}Test() ");
+
+            sb.AppendLine($"{{");
+
+            ParameterInfo paramMethod = method.GetParameters()[0];
+
+            sb.AppendLine($"{paramMethod.ParameterType} {paramMethod.Name} = new {paramMethod.ParameterType}();");
+
+
+
+            sb.AppendLine($"}}");
+
+            return sb.ToString();
+        }
+    }
+}
