4c477b43290e389b535f807d8e20d68fc3dceccd
STANISLAV KHAVERKO <stas.xaverko@gmail.com>
2023-10-07 13:34:43 +0200

    create manager generator

---
+++ Intotech.ReflectiveTools/SourceGenerators/StandardManagerGenerator/ManagerFullFlowGenerator.cs
@@ -0,0 +1,31 @@
+﻿using Intotech.Common.Bll.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intotech.ReflectiveTools.SourceGenerators.StandardManagerGenerator
+{
+    public class ManagerFullFlowGenerator
+    {
+        ManagerRenderer managerRenderer = new();
+
+        public virtual void GenerateFullStack(List<ModelBase> models, ManagerFullFlowEntity data)
+        {
+            foreach (var model in models)
+            {
+                if (model.GetType().Name.Contains("Context"))
+                {
+                    continue;
+                }
+                else
+                {
+                    managerRenderer.GenerateInterfaceBodyAndSave(model.GetType(), data.PathIManager, data.IManagerUsings, data.IManagerNamespace);
+                    managerRenderer.GenerateClassBodyAndSave(model.GetType(), data.PathManager, data.ManagerUsings, data.ManagerNamespace);
+                }
+                
+            }
+        }
+    }
+}
