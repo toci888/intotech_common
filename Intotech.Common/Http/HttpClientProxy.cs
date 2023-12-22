af3eb635ce6571f393173d7b1fd4317e05882d24
toci888 <bzapart@gmail.com>
2023-12-22 12:41:54 +0100

    authorization for api proxy enabled.

--- Intotech.Common/Http/HttpClientProxy.cs
+++ Intotech.Common/Http/HttpClientProxy.cs
@@ -1,25 +1,23 @@
 ﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Json;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Intotech.Common.Http
 {
     public abstract class HttpClientProxy
     {
         protected string BaseUrl = "http://127.0.0.1:8642/";
+        protected string AuthorizationToken;
 
-        protected virtual T ApiGet<T>(string url, bool isResponseArray)
+        public virtual T ApiGet<T>(string url, bool isResponseArray)
         {
             using (HttpClient hc = new HttpClient())
             {
                 hc.BaseAddress = new Uri(BaseUrl);
-                // hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", LoggedUserContext.User.Token);
+
+                if (!string.IsNullOrEmpty(AuthorizationToken))
+                {
+                    hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", AuthorizationToken);
+                }
 
                 HttpResponseMessage response = hc.GetAsync(url).Result;
 
@@ -39,15 +37,15 @@ namespace Intotech.Common.Http
             }
         }
 
-        protected virtual T ApiPost<T, TDto>(string url, TDto dto, bool isResponseArray)
+        public virtual T ApiPost<T, TDto>(string url, TDto dto, bool isResponseArray)
         {
             using (HttpClient hc = new HttpClient())
             {
                 hc.BaseAddress = new Uri(BaseUrl);
 
-                //if (LoggedUserContext.User != null)
+                if (!string.IsNullOrEmpty(AuthorizationToken))
                 {
-                   // hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", LoggedUserContext.User.Token);
+                    hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", AuthorizationToken);
                 }
 
                 HttpContent content = JsonContent.Create<TDto>(dto);
@@ -70,12 +68,16 @@ namespace Intotech.Common.Http
             }
         }
 
-        protected virtual T ApiPut<T, TDto>(string url, TDto dto, bool isResponseArray)
+        public virtual T ApiPut<T, TDto>(string url, TDto dto, bool isResponseArray)
         {
             using (HttpClient hc = new HttpClient())
             {
                 hc.BaseAddress = new Uri(BaseUrl);
-                // hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", LoggedUserContext.User.Token);
+
+                if (!string.IsNullOrEmpty(AuthorizationToken))
+                {
+                    hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", AuthorizationToken);
+                }
 
                 HttpContent content = JsonContent.Create<TDto>(dto);
 
@@ -98,12 +100,16 @@ namespace Intotech.Common.Http
             }
         }
 
-        protected virtual T ApiPatch<T, TDto>(string url, TDto dto, bool isResponseArray)
+        public virtual T ApiPatch<T, TDto>(string url, TDto dto, bool isResponseArray)
         {
             using (HttpClient hc = new HttpClient())
             {
                 hc.BaseAddress = new Uri(BaseUrl);
-                // hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", LoggedUserContext.User.Token);
+
+                if (!string.IsNullOrEmpty(AuthorizationToken))
+                {
+                    hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", AuthorizationToken);
+                }
 
                 HttpContent content = JsonContent.Create<TDto>(dto);
 
@@ -126,12 +132,16 @@ namespace Intotech.Common.Http
             }
         }
 
-        protected virtual T ApiDelete<T, TDto>(string url, TDto dto, bool isResponseArray)
+        public virtual T ApiDelete<T, TDto>(string url, TDto dto, bool isResponseArray)
         {
             using (HttpClient hc = new HttpClient())
             {
                 hc.BaseAddress = new Uri(BaseUrl);
-                // hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", LoggedUserContext.User.Token);
+
+                if (!string.IsNullOrEmpty(AuthorizationToken))
+                {
+                    hc.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", AuthorizationToken);
+                }
 
                 //  HttpContent content = JsonContent.Create<TDto>(dto);
 
@@ -154,5 +164,6 @@ namespace Intotech.Common.Http
             }
         }
 
+        protected abstract void CreateBearerTokenAuthorization(string email, string name, string surname, string roleName);
     }
 }
